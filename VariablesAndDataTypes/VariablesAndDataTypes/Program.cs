f333e3e674a6db5ef72a9bd847c359a6a622595e
mkunnari1 <mkunnari1@gmail.com>
2019-09-04 11:43:25 -0700

    New files for class

---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a simpe program
+
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("your name is " + yourName + "!");
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite nubmer?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("your favorite number + 5 is " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 47;
+            sbyte currentTemp = -23;
+            char questionMark = '\u2103'; //this is the unicode character for a question mark.
+
+            decimal moneyInBank = 100.5m;
+
+            double heightinCm = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temponMars = -341;
+
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+            bool isRaining = true;
+            string rainStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ page57drill/page57drill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page57drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What course are you on?\n");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number are you on?\n");
+            ushort coursePageNum = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".\n");
+            bool needsHelp = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.\n");
+            string posExper = Console.ReadLine();
+            Console.WriteLine("Is there any other feecback you'd like to provide? Please be specific.\n");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?\n");
+            ushort studyHours = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}

558a19a4c418ffb3065eb6e0b15e66cc4c91e132
mkunnari1 <mkunnari1@gmail.com>
2019-09-05 13:32:27 -0700

    New demo and New assignment

---
+++ branchingDemo/branchingDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace branchingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int curtemp = 71;
+            int roomtemp = 70;
+
+            if(curtemp == roomtemp)
+            {
+                Console.WriteLine("it is exactly room temp.");
+            }
+            else if(curtemp > roomtemp)
+            {
+                Console.WriteLine("it is warmer than room temp.");
+            }
+            else
+            {
+                Console.WriteLine("it is not exactly room temp.");
+            }
+
+            Console.ReadLine();
+
+            //one line if statement where the left side of the : is for true and the right side is for false see below for details.
+            string compResult = curtemp == roomtemp ? "It is room temp" : "it is not room temp";
+            Console.WriteLine(compResult);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ page92Drill/page92Drill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page92Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight.");
+            int pckwght = Convert.ToInt32(Console.ReadLine());
+            if(pckwght > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+            }
+            Console.WriteLine("Please enter the package width.");
+            int pckwdth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height");
+            int pckhght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height");
+            int pcklen = Convert.ToInt32(Console.ReadLine());
+            if ((pckwdth + pckhght + pcklen) > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+            }
+            int totalCost = ((pckwdth + pckhght + pcklen) * pckwght) / 100;
+            Console.WriteLine("Estimated shipping cost for this package is: $" + totalCost);
+            Console.ReadLine();
+        }
+    }
+}

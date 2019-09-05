d6b9677acf2d0479c946a890dd9876d94b433a3c
mkunnari1 <mkunnari1@gmail.com>
2019-09-05 14:10:49 -0700

    Drills for Class

---
+++ LoopsDemo/LoopsDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 47;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62 try again!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29 tyr again!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55 try again!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 47:
+                        Console.WriteLine("You guessed 47 good job!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are Wrong!!!!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+            
+
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ page94Drill/page94Drill/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page94Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There would you like to play a game?");
+            string playGame = Console.ReadLine();
+            do
+            {
+                if (playGame == "no" || playGame == "No")
+                {
+                    Console.WriteLine("I see... Well that is disapointing...");
+                    Console.ReadLine();
+                    System.Environment.Exit(1);
+                }
+                else if (playGame != "yes" || playGame != "Yes")
+                {
+                    Console.WriteLine("I didn't understand that, please answer yes or no.");
+                    playGame = Console.ReadLine();
+                }
+            }
+            while (playGame != "Yes" || playGame != "yes");
+
+            Console.WriteLine("Wonderful! Let's get started.");
+            Console.WriteLine("First pick a number between 48 and 70");
+            int pickNum = Convert.ToInt32(Console.ReadLine());
+            bool winStip = pickNum == 47;
+
+            while (winStip != true)
+            {
+                switch() 
+                
+            }
+        }
+    }
+}

ddea88e1121fa45f7a4bcb7aa89c91d1894adbdf
mkunnari1 <mkunnari1@gmail.com>
2019-09-09 13:25:25 -0700

    new drill

--- page106Drill/page106Drill/Program.cs
+++ page106Drill/page106Drill/Program.cs
@@ -10,6 +10,34 @@ namespace page106Drill
     {
         static void Main(string[] args)
         {
+            try
+            { 
+                List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 123, 456, 789, 741, 852, 963 };
+
+                Console.WriteLine("Welcome user to the Thuder Dome!!! \noh... I see, yes yes I got it. \nWhat I meant to say was \nWelcome user to the division game!!! \nProvide me with a number and I will use it to divide all the numbers on my list by. \nPretty neat yes?");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You provided me with the number " + userInput + " I will now do great math!");
+                foreach(int number in numbers)
+                {
+                    int answer = number / userInput;
+                    Console.WriteLine(answer);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("My dude that is not a number, please input a number");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You can't divide by zero friend, that is not how math works");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("I have emerged from the try/catch block and continued with the program execution! That's basically a win.");
+            Console.ReadLine();
+            
         }
     }
 }

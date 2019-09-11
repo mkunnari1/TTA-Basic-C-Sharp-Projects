f98c75eb61a2eb17eaf5d0674042268dae458a4a
mkunnari1 <mkunnari1@gmail.com>
2019-09-11 14:01:05 -0700

    New Drill

---
+++ page114Drill/page114Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page114Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(mathClass.DoIT(12));
+            Console.WriteLine(mathClass.DoIT(num1: 6.2698m));
+            Console.WriteLine(mathClass.DoIT(num1: "4"));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ page114Drill/page114Drill/mathClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page114Drill
+{
+    public class mathClass
+    {
+        public static int DoIT (int num1)
+        {
+            int num2 = num1 / 2;
+            return num2;
+        }
+
+        public static decimal DoIT( decimal num1)
+        {
+            int num2 = Decimal.ToInt32(num1 * 2);
+            return num2;
+        }
+
+        public static string DoIT( string num1)
+        {
+            int num2 = Convert.ToInt32(num1);
+            int num3 = num2 + 5;
+            string num4 = Convert.ToString(num3);
+            return num4;
+            
+        }
+    }
+}

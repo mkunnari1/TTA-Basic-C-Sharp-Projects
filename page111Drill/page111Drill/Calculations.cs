ced2418395e1194252fc3d587e9f728b976c85f5
mkunnari1 <mkunnari1@gmail.com>
2019-09-10 17:15:21 -0700

    New Drill

--- page111Drill/page111Drill/Calculations.cs
+++ page111Drill/page111Drill/Calculations.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace page111Drill
 {
-    class Calculations
+    public class Calculations
     {
+        public static int Additions()
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int answer1 = userInput + 10;
+            return answer1;
+            
+        }
+
+        public static int Multiply()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int answer1 = userInput * randomNumber;
+            return answer1;
+        }
+
+        public static int wholeLottaMath()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if (randomNumber <= userInput)
+            {
+                int tempNum = userInput * randomNumber;
+                tempNum = tempNum * randomNumber;
+                int answer1 = tempNum / 15;
+                return answer1;
+            }
+            else
+            {
+                int tempNum2 = userInput / randomNumber;
+                tempNum2 = tempNum2^3;
+                int answer2 = tempNum2;
+                return answer2;
+            }
+
+            
+        }
     }
 }
--- page111Drill/page111Drill/Program.cs
+++ page111Drill/page111Drill/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace page111Drill
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Hail and well met weary user.  \nYou have arrived at the kingdom of random math operations. " +
+               "\nPlease supply me with a number!");
+                Console.WriteLine("I hath taken the number you bestowed upon me and added 10 to it! \nThe answer is: " +Calculations.Additions());
+                Console.WriteLine("\nI have yet to show you my true numeric powers! \nGIVE ME ANOTHER NUMBER!!!!!!");
+                Console.WriteLine("Your number has been multiplied by a random number between 0 and 100!!! \nBehold the glory of math: "
+                    + Calculations.Multiply());
+                Console.WriteLine("\nStill don't see the majestic nature of mathmatics?\nANOTHER NUMBER NAIVE!!!!!!");
+                Console.WriteLine("GAZE UPON MY MIGHT!!!! MATHHHHHHH HAS BEEN DONE!\nBOW BEFORE OUR NUMBERS PEASANT!\n" + Calculations.wholeLottaMath());
+                Console.WriteLine("Thank you for visiting the kingdom of random math operations have a great day!");
+
+
+                
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Bruh... those are letters. Please leave my kingdom... \nYOU HAVE DIED\nGAME OVER");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+           
         }
     }
 }

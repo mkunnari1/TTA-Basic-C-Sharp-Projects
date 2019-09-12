8a277bab61dc30f6d238641fdec82e795cbd3701
mkunnari1 <mkunnari1@gmail.com>
2019-09-11 19:01:00 -0700

    Update to blackjack and new drill

---
+++ page117Drill/page117Drill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page117Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number.");
+            mathClass.Divisor();
+            Console.ReadLine();
+
+            int m;
+            int k;
+
+            mathClass.Divisor(out m, out k);
+
+            Console.WriteLine("The value of m is now {0}", m);
+            Console.WriteLine("The value of k is now {0}", k);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ page117Drill/page117Drill/mathClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page117Drill
+{
+    public class mathClass
+    {
+        public static void Divisor()
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = num1 / 2;
+            Console.WriteLine("You entered " + num1 + ", divide that by 2 and we get: " + num2);
+        }
+
+        public static void Divisor(out int a, out int b)
+        {
+            a = 15;
+            b = 25;
+            a += a;
+            b += b;
+        }
+    }
+}

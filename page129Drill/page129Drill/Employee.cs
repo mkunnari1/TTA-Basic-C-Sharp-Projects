f682fe0c6b29325ef8b0d3f9335a38ea1e7fb968
mkunnari1 <mkunnari1@gmail.com>
2019-09-18 21:03:26 -0700

    add drill

---
+++ page129Drill/page129Drill/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page129Drill
+{
+    public class Employee
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int empId { get; set; }
+
+        public static Employee operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.empId == employee2.empId)
+            {
+                Console.WriteLine("The employee ID's are a match");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+                return employee;
+            }
+            else
+            {
+                Console.WriteLine("The employee ID's are NOT a match");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+                return employee;
+            }
+        }
+
+        public static Employee operator !=(Employee employee, Employee employee2)
+        {
+            if (employee.empId != employee2.empId)
+            {
+                Console.WriteLine("The employee ID's are NOT a match");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+                return employee;
+            }
+            else
+            {
+                Console.WriteLine("The employee ID's are a match");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+                return employee;
+            }
+        }
+
+    }
+}
---
+++ page129Drill/page129Drill/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page129Drill
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee();
+            employee.empId = 1;
+
+            Employee employee2 = new Employee();
+            employee2.empId = 2;
+
+            Console.WriteLine(employee == employee2);
+            Console.ReadLine();
+        }
+           
+           
+      
+    }
+}

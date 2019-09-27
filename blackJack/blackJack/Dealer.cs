318d968b7266305ed05622809a8c64a28c302669
mkunnari1 <mkunnari1@gmail.com>
2019-09-27 13:38:52 -0700

    New Drill

--- blackJack/blackJack/Dealer.cs
+++ blackJack/blackJack/Dealer.cs
@@ -20,6 +20,7 @@ namespace blackJack
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\HP\OneDrive\Documents\C#-Projects\TTA-Basic-C-Sharp-Projects\blackJack\cardLog.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
                 Deck.Cards.RemoveAt(0);
---
+++ page156Drill/page156Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page156Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("\nHey There can i get a number?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be " + DateTime.Now.AddHours(userInput), userInput);
+            Console.ReadLine();
+        }
+    }
+}

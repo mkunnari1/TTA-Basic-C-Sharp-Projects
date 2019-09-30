5b811a3311b90751d569ce260f4dd0456a645128
mkunnari1 <mkunnari1@gmail.com>
2019-09-30 11:37:22 -0700

    New Drill

--- page162Drill/page162Drill/Dude.cs
+++ page162Drill/page162Drill/Dude.cs
@@ -6,16 +6,23 @@ using System.Threading.Tasks;
 
 namespace page162Drill
 {
-    class Dude
+    public class Dude
     {
-        string Name { get; set; }
-        string Id { get; set; }
-        int Radness { get; set; }
-
-        public static void MyDude(string Name, string Id, int Radness )
+        public Dude(string name) : this(name, 5)
         {
-            Console.WriteLine(Name + " is my dude, his dude ID is: " + Id + " and his current level of radness is: " + Radness);
-            Console.ReadLine();
+            
         }
+        public Dude(string name, int radness)
+        {
+            Name = name;
+            Radness = radness;
+            Id = Guid.NewGuid();
+        }
+
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        public int Radness { get; set; }
+
+       
     }
 }
--- page162Drill/page162Drill/Program.cs
+++ page162Drill/page162Drill/Program.cs
@@ -10,6 +10,13 @@ namespace page162Drill
     {
         static void Main(string[] args)
         {
+            const string bro = " THIS GUY IS A TOTAL BRO";
+
+            var myGuy = new Dude("John");
+
+
+            Console.WriteLine(myGuy.Name + " has a radness level of: " + myGuy.Radness + " and his bro ID is: "+ myGuy.Id+ bro) ;
+            Console.ReadLine();
         }
     }
 }

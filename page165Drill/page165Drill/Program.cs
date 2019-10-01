3a0e5ad963ba7202609698d4763087fcd13d6f66
mkunnari1 <mkunnari1@gmail.com>
2019-10-01 13:01:25 -0700

    update drill

--- page165Drill/page165Drill/Program.cs
+++ page165Drill/page165Drill/Program.cs
@@ -11,10 +11,37 @@ namespace page165Drill
         static void Main(string[] args)
         {
             Console.WriteLine("Hey there user, I forget how old you are. What age did you turn or will turn this year?");
-            int userAge = Convert.ToInt32(Console.ReadLine());
-            int yearOfBirth = DateTime.Now.Year - userAge;
-            Console.WriteLine("Okay cool so you were born in " + yearOfBirth + " right?");
-            Console.ReadLine();
+
+            try
+            {
+                uint userAge = Convert.ToUInt32(Console.ReadLine());
+                if (userAge == 0)
+                {
+                    Console.WriteLine("ummmm excuse me, but there is no way that you are 0 years old. get outta here.");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    int yearOfBirth = DateTime.Now.Year - Convert.ToInt32(userAge);
+                    Console.WriteLine("So that means you were born in " + yearOfBirth + "!");
+                    Console.ReadLine();
+                        
+                }
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Positive numbers only, you can't be born in the future and be using this program today.");
+                Console.ReadLine();
+                return;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter your age in digits only.");
+                Console.ReadLine();
+                return;
+            }
         }
     }
 }

7469680e0e6111a688cd61c39e95d79ea64e98b6
mkunnari1 <mkunnari1@gmail.com>
2019-10-08 14:09:38 -0700

    New Projects

---
+++ MvcTutorial/MvcTutorial/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using MvcTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //things you can do within controllers
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\HP\OneDrive\Documents\C#-Projects\TTA-Basic-C-Sharp-Projects\MvcTutorial\Log.txt", text);
+
+            //Random ran = new Random(10);
+            //int num = ran.Next();
+
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //viewbag.randomnumber = num;
+
+            //return Content("Hello!");
+
+            //return RedirectToAction("Contact");
+
+            //throw new Exception("Hey Baunch this page isn't working anymore!");
+
+            //List<string> names = new List<string>
+            //{
+            //    "jesse",
+            //    "joe",
+            //    "toney"
+            //};
+
+            //return View(names);
+            User user = new User();
+            user.ID = 1;
+            user.FirstName = "Joe";
+            user.LastName = "Tony";
+            user.Age = 47;
+
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TechAcadStudentsMVC/TechAcadStudentsMVC/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechAcadStudentsMVC.Models;
+
+namespace TechAcadStudentsMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() // action result can be thought of as "rendered HTML" 
+        {
+            return View(); // the view method looks for the view file that the method name calls for in this case it Index() 
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Page - The Tech Academy";
+
+            return View();
+        }
+
+        public ActionResult Instructors()
+        {
+            List<Instructor> instructors = new List<Instructor>
+            {
+                new Instructor
+                {
+                    Id=1,
+                    FirstName = "Daithi",
+                    LastName = "DeNogla"
+                },
+                new Instructor
+                {
+                    Id=2,
+                    FirstName = "BigJiggly",
+                    LastName = "Panda"
+
+                },
+                new Instructor
+                {
+                    Id= 3,
+                    FirstName="Joe",
+                    LastName="Shmoe"
+                }
+            };
+            return View(instructors);
+        }
+        public ActionResult Instructor()
+        {
+            return View();
+        }
+    }
+}
